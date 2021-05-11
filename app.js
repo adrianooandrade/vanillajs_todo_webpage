0736936a92eef776d9297da81a9117b1d3b192f5
Adriano <allonan.619@gmail.com>
2021-05-11 02:57:56 +0100

    local variables saved
    input text is not checked
    filters are working
    delete still deletes the first match found
    loading checked todos still missing

--- app.js
+++ app.js
@@ -3,17 +3,22 @@
 const Input = document.querySelector('.input-text')
 const AddButton = document.querySelector('.submit-button')
 const List = document.querySelector('.list')
-const Option = document.querySelector('.filter')
+const Filter = document.querySelector('.filter')
 
 
 //Event Listeners
 AddButton.addEventListener('click', addTodo)
-List.addEventListener('click', deleteCheck)
-Option.addEventListener('click', filterTodo)
-
+List.addEventListener('click', checkClick)
+Filter.addEventListener('change', filterTodo)
+document.addEventListener('DOMContentLoaded',getTodos)
 //Functions
 
 function addTodo(event) {
+
+    if(Input.value==""){
+        alert("Write something")
+        return
+    }
     //Todo div
     const newDiv = document.createElement('div')
     newDiv.classList.add('todo')
@@ -23,7 +28,6 @@ function addTodo(event) {
     newTodo.innerText = Input.value
     newTodo.classList.add('todo-item')
     newDiv.appendChild(newTodo)
-    
    
     //Check button
     const completedButton = document.createElement('button')
@@ -40,60 +44,124 @@ function addTodo(event) {
     //Append to todo list
     List.appendChild(newDiv)
     
+    saveTodos(Input.value)
+
     //Clear input
     Input.value=""
 
 }
 
-function deleteCheck(e){
-    
-    const item = e.target
-    
-    if(item.classList[0] === "delete-btn"){
-        const parent = item.parentElement
-        parent.classList.add("fall")
-        parent.addEventListener("transitionend",function(){
-            parent.remove()
-        });
-    }
+function checkClick(e){
 
+    const item = e.target
     if(item.classList[0] === "check-btn"){
         const parent = item.parentElement
         parent.classList.toggle("checked")
     }
 
+    if(item.classList[0] === "delete-btn"){
+        const parent = item.parentElement
+        parent.classList.add("fall")
+        deleteTodos(parent)
+        parent.addEventListener("transitionend",function(){
+            parent.remove()
+        });
+    }
+
 }
 
 function filterTodo(e){
+    //div.todo 
+    //div.todo.checked
 
-    const filtroList = List.childNodes
-
-    filtroList.forEach(function(todo){
+    const todos = List.childNodes
+    console.log(todos)
+    todos.forEach(function(child){
         switch(e.target.value){
             case "all":
-                todo.style.display = "flex";
+                child.style.display = "flex";
                 break
             case "completed":
-                if(todo.classList.contains('completed')) {
-                    todo.style.display = 'flex'
+                if(child.classList.contains('checked')) {
+                    child.style.display = "flex";
                 }
                 else{
-                    todo.style.display = 'none';
+                    child.style.display = "none";
                 }
                 break
             case "uncompleted":
-                if(todo.classList.contains('uncompleted')) {
-                    todo.style.display = 'flex'
+                if(!child.classList.contains('checked')) {
+                    child.style.display = "flex";
                 }
                 else{
-                    todo.style.display = 'none';
+                    child.style.display = "none";
                 }
                 break
         }
-    });
+    })
 
 }
 
-function saveTodos(todo){
+function saveTodos(tarefa){
+    let todos
+    if(localStorage.getItem('lista') === null){
+        todos = []
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("lista"))
+    }
+    todos.push(tarefa)
+    localStorage.setItem("lista", JSON.stringify(todos))
+}
+
+function getTodos(){
+    let todos
+    if(localStorage.getItem('lista') === null){
+        todos = []
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("lista"))
+    }
+    todos.forEach(function(todo){
+        const newDiv = document.createElement('div')
+        newDiv.classList.add('todo')
+
+        //Create list
+        const newTodo = document.createElement('li') //innerHTML
+        newTodo.innerText = todo
+        newTodo.classList.add('todo-item')
+        newDiv.appendChild(newTodo)
+
+        //Check button
+        const completedButton = document.createElement('button')
+        completedButton.setAttribute("class","check-btn")
+        completedButton.innerHTML = '<img class ="check-img" src = check.png>'
+        newDiv.appendChild(completedButton)
+
+        //Delete button
+        const deleteButton = document.createElement('button')
+        deleteButton.setAttribute("class","delete-btn")
+        deleteButton.innerHTML = '<img class ="delete-img" src = delete.png>'
+        newDiv.appendChild(deleteButton)
+
+        //Append to todo list
+        List.appendChild(newDiv)
+    })
+    localStorage.setItem("lista", JSON.stringify(todos))
+}
+
+function deleteTodos(tarefa){
+    let todos
+    if(localStorage.getItem('lista') === null){
+        todos = []
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("lista"))
+    }
+    const index = tarefa.children[0].innerText;
+    console.log(index)
+    todos.splice(todos.indexOf(index),1)
+    localStorage.setItem("lista", JSON.stringify(todos))
 
 }
+
